6908136f3ec158fff8b0a2522048e23d47267b7b
evanandrewson <evanandrewson@gmail.com>
2019-11-18 17:20:32 -0800

    added button to render new quote

--- src/containers/SimpsonsQuote.js
+++ src/containers/SimpsonsQuote.js
@@ -4,6 +4,7 @@ import Quote from '../components/quote/Quote';
 import { connect } from 'react-redux';
 import { getLoading, getCharacterImage, getCharacterName, getQuote } from '../selectors/quoteSelectors';
 import { setQuotePromise } from '../actions/quoteDataActions';
+import Load from '../components/quote/Load';
 
 class SimpsonsQuote extends Component {
   componentDidMount() {
@@ -12,14 +13,20 @@ class SimpsonsQuote extends Component {
 
   render() {
     if(this.props.loading) return <h1>Loading...</h1>;
-    return <Quote quote={this.props.quote} characterName={this.props.characterName} characterImage={this.props.characterImage}  />;
+    return (
+      <>
+        <Quote quote={this.props.quote} characterName={this.props.characterName} characterImage={this.props.characterImage}  />
+        <Load fetchQuote={this.props.fetchQuote} />
+      </>
+    );
   }
 
   static propTypes = {
     quote: PropTypes.string.isRequired,
     characterName: PropTypes.string.isRequired,
     characterImage: PropTypes.string.isRequired,
-    fetchQuote: PropTypes.func.isRequired
+    fetchQuote: PropTypes.func.isRequired,
+    loading: PropTypes.bool.isRequired
   }
 }
 
